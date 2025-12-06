cf2fac220571da74047bfe14f3432611652be0db
Salaheddine <salahsimbaa@gmail.com>
2025-12-06 10:19:15 +0100

    Lasr touches

---
+++ src/constants/index.js
@@ -0,0 +1,296 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "StEco platform",
+    description:
+      "During a one month , I worked on the development of Steco, a web-based stock management application. The objective of the project was to build a modern solution that helps companie manage and track their inventory in real time.",
+    subDescription: [
+      "Steco is a modern web-based stock management platform designed to help companies efficiently monitor",
+      "track, and control their inventory in real time. The application simplifies daily stock operations by providing a clean and intuitive interface built with Vue.js on the frontend and a powerful, scalable Laravel backend.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/StEco.png",
+    tags: [
+      {
+        id: 1,
+        name: "Laravel",
+        path: "/assets/logos/Laravel.png",
+      },
+      {
+        id: 2,
+        name: "Vue.js",
+        path: "/assets/logos/Vue.png",
+      },
+      {
+        id: 2,
+        name: "Vitejs",
+        path: "/assets/logos/vitejs.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Speed",
+    description:
+      "Speed is a modern web application designed to optimize the fast delivery of store products.",
+    subDescription: [
+      "The platform allows users to order items from a store and receive them quickly through an efficient delivery workflow. The system combines a clean user experience with a powerful backend built using Laravel, JavaScript, and CSS.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/Speed.png",
+    tags: [
+      {
+        id: 1,
+        name: "Laravel",
+        path: "/assets/logos/Laravel.png",
+      },
+      {
+        id: 2,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 4,
+        name: "MySQL",
+        path: "/assets/logos/mysql.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Absence Management",
+    description:
+      "The goal of the application is to simplify and automate absence tracking, allowing organizations to easily record, monitor, and analyze employee absence data while keeping everything stored locally.",
+    subDescription: [
+      "I worked in a team of 4 developers to build a desktop application for managing employee absences using Electron, completed in 15 days. The project uses a lightweight and efficient stack based on JavaScript, Node.js, CSS, and SQLite, and delivers a clean and intuitive interface",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/electrone.png",
+    tags: [
+      {
+        id: 1,
+        name: "Electron",
+        path: "/assets/logos/Eelectrone.png",
+      },
+      {
+        id: 2,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "SQLite",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "Node.js",
+        path: "/assets/logos/nodejs.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Ista School Website",
+    subDescription: [
+      "I worked with a team of 6 developers to design and build a modern school website. The platform presents information about the school, its programs, activities, students, and services using a clean and modern UI. The project is fully developed using JavaScript and CSS, with a strong focus on responsive design and user experience.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/ISTAé.png",
+    tags: [
+      {
+        id: 1,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 2,
+        name: "git",
+        path: "/assets/logos/git.svg",
+      },
+            {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+            {
+        id: 2,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "3D Web Site",
+    subDescription: [
+      "I developed a personal portfolio website with 3D interactive elements, focusing on modern web design and smooth user experience. The project showcases my skills, projects, and background using dynamic JavaScript features and 3D visual effects, creating an immersive and attractive interface.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/3d.png",
+    tags: [
+      {
+        id: 1,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 2,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 4,
+        name: "Vite.js",
+        path: "/assets/logos/vitejs.svg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Car Rental",
+    subDescription: [
+      "I developed a web application for car rental and booking using React for the frontend and Laravel for the backend. The project allows users to browse available cars, book rentals, and manage their reservations efficiently, while providing an admin panel for managing the fleet and users.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/voiture.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Laravel",
+        path: "/assets/logos/Laravel.png",
+      },
+            {
+        id: 3,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+
+      {
+        id: 4,
+        name: "mysql",
+        path: "/assets/logos/mysql.png",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/ali-sanati/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software Developer",
+    job: "Security & Defense Projects",
+    date: "2021-2023",
+    contents: [
+      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
+      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
+      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
+      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
+      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
+    ],
+  },
+  {
+    title: "Back-End Developer",
+    job: "Car Manufacture",
+    date: "2023-2024",
+    contents: [
+      "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
+      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
+      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
+      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
+      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2025-Present",
+    contents: [
+      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
